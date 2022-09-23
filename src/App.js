b3caf6ce71af9673f8a15d6255df5d34ef077f3b
David Murillo <david.murillo@ascendle.com>
2022-09-23 17:46:11 -0500

    button, input created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import { Form, Formik } from 'formik';
 import styled from 'styled-components';
 import './App.css';
+import Input from './components/Input/Input';
+import Button from './components/Button/Button';
 
 export default function App() {
 	return (
 		<Container>
 			<Section>
-
+				<Formik initialValues={{ deposit: '', contribution: '', years: '', rate: '' }} onSubmit={handleSubmit}>
+					<Form>
+						<Input name='deposit' label='Deposito inicial' />
+						<Input name='contribution' label='Contribucion anual' />
+						<Input name='years' label='Anios' />
+						<Input name='rate' label='Interes estimado' />
+						<Button>Calcular</Button>
+					</Form>
+				</Formik>
 			</Section>
 		</Container>
 	);
 }
+
+function handleSubmit() { }
 
 const Container = styled.div`
 	display:flex;
